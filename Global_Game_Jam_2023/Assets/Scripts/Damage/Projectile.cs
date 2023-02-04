8e4722abb7115dbb092ab17f8f0f828374baa92e
Samuele-Angeletti <104771865+Samuele-Angeletti@users.noreply.github.com>
2023-02-04 10:28:38 +0100

    fixed projectile

--- Global_Game_Jam_2023/Assets/Scripts/Damage/Projectile.cs
+++ Global_Game_Jam_2023/Assets/Scripts/Damage/Projectile.cs
@@ -22,4 +22,15 @@ public class Projectile : MonoBehaviour
     {
         _rigidbody.velocity = speed * Time.fixedDeltaTime * transform.up;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+
+    }
 }
