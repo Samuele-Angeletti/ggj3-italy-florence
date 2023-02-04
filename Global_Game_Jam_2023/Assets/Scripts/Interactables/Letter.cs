f35ef16fb4693e57df62143c1cb34747f9efec4c
Samuele-Angeletti <104771865+Samuele-Angeletti@users.noreply.github.com>
2023-02-05 00:44:31 +0100

    alphabet

---
+++ Global_Game_Jam_2023/Assets/Scripts/Interactables/Letter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Letter : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer spriteRenderer;
+
+    public void Initialize(Sprite sprite)
+        { spriteRenderer.sprite = sprite; }
+}
